ba66e2ce3662c9b7daeece62a4eca55a2a454717
nyhunter77 <nyhunter77@yahoo.com>
2014-10-08 17:17:49 -0400

    Using current user correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_session
+  def current_user
     session[:user_id] ? User.find(session[:user_id]) : nil
   end
 
