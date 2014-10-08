53d117d404b48dd4c1d4df2aa9f4b3d3e81c28b8
Nathan Brown <nkabrown@gmail.com>
2014-10-08 14:29:47 -0400

    Created sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.where(username: params[:username]).first
+    if @user && @user[:password] == params[:password]
+      flash[:notice] = "Welcome to the Newsroom."
+      session[:user_id] = @user.id
+      redirect_to users_path
+      puts "**********"
+      puts session[:user_id]
+      puts "**********"
+    else
+      flash[:notice] = "There was a problem signing you in. Please try again."
+      render :new
+    end
   end
 
   def destroy
