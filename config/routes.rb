7bb828e9643af71d06d1b569c6bafc6a3cf01b87
nyhunter77 <nyhunter77@yahoo.com>
2014-10-08 12:28:06 -0400

    updating - working on form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users
+  resources :posts do
+    resources :comments
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'users#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
