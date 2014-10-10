0c5877e9f79343b9580c835db3cfb1ad5fcec33a
Nathan Brown <nkabrown@gmail.com>
2014-10-10 11:02:00 -0400

    Add paperclip functionality and images to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:fname, :lname, :email, :username, :password)
+    params.require(:user).permit(:fname, :lname, :email, :username, :password, :avatar)
   end
 
   def set_user
