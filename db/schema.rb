3f83dac04ae2502e73dc0f00b19f56912d3b1960
Nathan Brown <nkabrown@gmail.com>
2014-10-10 10:33:54 -0400

    Add paperclip gem and generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009150247) do
+ActiveRecord::Schema.define(version: 20141010143028) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20141009150247) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
